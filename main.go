1f912a9e418b772fbb49734e35e961aee9fca7b1
wakametarou <wakame.programing@gmail.com>
2022-03-28 02:25:35 +0900

    ファイルの生成機能を作成 #1

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	flag.Parse()
 	arg := flag.Arg(0)
-	fmt.Printf("Hello %s\n", arg)
+	msg := fmt.Sprintf("Hello %s\n", arg)
+
+	f, err := os.Create("./hello.txt")
+	if err != nil {
+		fmt.Printf("failed to create file \n: %v", err)
+		return
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(msg)
+	if err != nil {
+		fmt.Printf("failed to write message to file \n: %v", err)
+		return
+	}
+
+	fmt.Println("Done!")
 }
