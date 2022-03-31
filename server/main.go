ba4a96bc3d024426e8c11a1ea69b19865f9447d8
wakametarou <wakame.programing@gmail.com>
2022-03-31 17:14:43 +0900

    QRコード作成のAPIを追加 #4

--- server/main.go
+++ server/main.go
@@ -2,9 +2,57 @@ package main
 
 import (
 	"fmt"
+	"image/png"
 	"log"
 	"net/http"
+	"strconv"
+
+	"go-qr-app/qrgen"
 )
+
+func generateQRCode(w http.ResponseWriter, r *http.Request) {
+	param := r.URL.Query()
+
+	url := param.Get("url")
+	if url == "" {
+		http.Error(w, "missing url", http.StatusBadRequest)
+		return
+	}
+
+	widthStr := param.Get("width")
+	width, err := strconv.Atoi(widthStr)
+	if err != nil {
+		http.Error(w, "invalid width", http.StatusBadRequest)
+		return
+	}
+
+	heightStr := param.Get("height")
+	height, err := strconv.Atoi(heightStr)
+	if err != nil {
+		http.Error(w, "invalid height", http.StatusBadRequest)
+		return
+	}
+
+	// validation
+	if width <= 45 {
+		http.Error(w, "width must be more than 45px", http.StatusBadRequest)
+		return
+	}
+	if height <= 45 {
+		http.Error(w, "height must be more than 45px", http.StatusBadRequest)
+		return
+	}
+
+	img, err := qrgen.GenQRCode(url, width, height)
+	if err != nil {
+		errStr := fmt.Sprintf("failed to generate QR code %v", err)
+		http.Error(w, errStr, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
+}
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	msg := r.URL.Query().Get("message")
@@ -13,6 +61,8 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/generate", generateQRCode)
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalf("failed to create server: %v", err)
